b37746b3c9f26594165c50bca1deb23a5f172fd2
huggo <victorhuggocontato42@gmail.com>
2023-09-15 08:33:57 -0300

    feat: solution to problem 02

--- p02.ml
+++ p02.ml
@@ -6,11 +6,20 @@
 - : (string * string) option = None
 *)
 
-let rec lastTwo (xs: 'a list): 'a option list =
+(*
+    how to define a tuple ->
+    type: int * int
+    value 1, 2
+    list with one element: [1, 2] = [(1, 2)] -> don't need to use ()
+    list with two elements [1; 2]
+*)
+
+let rec last_two (xs: 'a list): ('a * 'a) option =
     match xs with
     | [] -> None
-    | [x, y] -> Some x, y
-    | rest :: _ -> last rest
+    | [_] -> None
+    | [x; y] -> Some (x, y)
+    | _ :: rest -> last_two rest
 
 let () = 
     print_endline "Hello world"
