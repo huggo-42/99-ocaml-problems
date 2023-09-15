ff01b4a26df6adc4ca1ff449611f54e555da2c57
huggo <victorhuggocontato42@gmail.com>
2023-09-15 08:33:44 -0300

    feat: solution to problem 01

--- p01.ml
+++ p01.ml
@@ -1,11 +1,15 @@
-(* 1. Write a function last : 'a list -> 'a option that returns the last element of a list. (easy)
+(*
+1. Write a function last : 'a list -> 'a option that returns the last element of a list. (easy)
 # last ["a" ; "b" ; "c" ; "d"];; 
 - : string option = Some "d"
 # last [];;
 - : 'a option = None
 *)
 
-(* last(xs: List<T>) -> Optional<T> *)
+(*
+last(xs: List<T>) -> Optional<T>
+*)
+
 let rec last (xs: 'a list): 'a option =
     match xs with
     | [] -> None
